f95542c036fc3e70b0e560e2859c7f0d444bf5ee
lbcamargo94 <lb.camargo94@gmail.com>
2022-02-28 21:49:51 -0300

    feat:episodes.js: Criado informaçoes adicionais dos episodios 1 ao 5 ✅

---
+++ src/data/episodes.js
@@ -0,0 +1,262 @@
+const EpisodesAddOn = [
+  // Season 1 - Episode 1
+  {
+    deaths: [
+      'Frank Palicky',
+      'Glenn',
+      'At least 5 unnamed Gromflomites',
+      'At least 5 unnamed aliens',
+    ],
+    description: '"Pilot" is the first episode of the first season of Rick and Morty. It is the first episode of the series overall. It premiered on December 2, 2013. It was written by Dan Harmon and Justin Roiland, and directed by Justin Roiland.',
+    errors: [
+      'At the end of the episode, when Rick was talking about his adventures with Morty, while he was having seizures, the garage door closed by itself.',
+    ],
+    id: 1,
+    image: 'https://i.pinimg.com/originals/d5/5a/b1/d55ab1f6b2e299b90035ad94ce56b96a.jpg',
+    locations: [
+      'Smith Residence',
+      'Harry Herpson High School',
+      'Dimension 35-C',
+      'Intergalactic Customs',
+    ],
+    name: '	Pilot',
+    synopsis: 'Rick takes Morty on a trip to another dimension to find seeds for "mega-trees," while Jerry and Beth argue over Ricks influence on their son.',
+    trivia: [
+      'The opening scene of the episode, in which a rambling Rick wakes up Morty and brings him onboard his newly-built Space Cruiser, is a parody of the 2006 Oor Wullie comic Joe Republic and the Lizards From Hell Part 1: First Things First by Seán Manz, with Rick and Morty in place of Oor Wullie and Joe Republic. In the comic, Oor Wullie wakes up Joe while rambling about lizards having kidnapped his wife Primrose, and brings him to his newly-bought UFO to fly to Dundee, revealing once in the air that rather than rescuing his wife, he plans to bring down the government by freeing the Broons.',
+
+      "In Mr. Goldenfold's math class, there's one particular background character in his class who has a T-shirt with a blue Bart Simpson head on it. The student is best visible in the scene were Morty is fondling Mr. Goldenfold and reappears as a recurring background character.",
+
+      'A Wilhelm scream is heard when Morty shoots one of the aliens with a laser, while Rick is trying to set up the portal and go back home.',
+
+      'After Morty has hidden the mega seeds and is coming out of the bathroom, he stands in line with Rick behind an alien that is very similar to a ferengi (a race frequently appearing in almost all Star Trek series) at around the 14:40 mark. The differences are the cloths (it has somewhat casual clothing compared to usually having very extravagant fashion sense in Star Trek), skin color (it is having yellow skin and an orange nose while they have beige colored skin and nose in the Star Trek universe).',
+
+      "The piles of numbers are featured in Morty's daydreams is likely a reference to the Looney Tunes short, From A to Z-Z-Z-Z.",
+
+      'Summer saying, "Frank Palicky was frozen to death today!" references the film, Suburban Commando.',
+    ],
+  },
+  // Season 1 - Episode 2
+  {
+    deaths: [
+      'Creepy Little Girl (in her dream)',
+      'Centaur (in his dream)',
+      'Mrs. Pancakes (in her dream)',
+      'Mr. Goldenfold (in his dream)',
+    ],
+    description: '"Lawnmower Dog" is the second episode of the first season of Rick and Morty. It is the second episode of the series overall. It premiered on December 9, 2013. It was written by Ryan Ridley and directed by John Rice.',
+    errors: 'unknown',
+    id: 2,
+    image: 'https://i.pinimg.com/originals/7b/1e/01/7b1e01b28c5cc0ee10c68276c20046e3.jpg',
+    locations: [
+      'Smith House',
+    ],
+    name: 'Lawnmower Dog',
+    synopsis: "Rick creates a device to make family dog Snuffles smarter, but it goes poorly. Rick and Morty invade the dreams of Morty's math teacher.",
+    trivia: [
+      'unknown',
+    ],
+  },
+  // Season 1 - Episode 3
+  {
+    deaths: [
+      "All of Ruben's tuberculosis",
+      "Alexander",
+      "Ruben's Gonorrhea",
+      "Poncho",
+      "Roger",
+      "Dr. Xenon Bloom",
+      "Ruben's Hepatitis A",
+      "All of Ruben's E. coli",
+      "Ruben's Hepatitis C",
+      "Ruben",
+    ],
+    description: '"Anatomy Park" is the third episode of the first season of Rick and Morty. It is the third episode of the series overall. It premiered on December 16, 2013. It was written by Eric Acosta and Wade Randolph, and directed by John Rice.',
+    errors: [
+      'As Doctor Bloom is going to operate the bone train, several viruses invade, which he calls an "E. coli outbreak.",However, E. coli are bacteria, not viruses. Ironically, the viruses are Enterobacteria Phage T4, viruses that infect and kill E. coli bacteria.',
+
+      "The first clock appearance in the garage is set to 3:00. Later, at the dinner table, the clock is set at 6:00. Then, back in the garage, the clock is set to 2:00. Then after Rick, Morty, and Annie return from space, the clock in the garage is set to 3:00.",
+
+      "Hawaii is disproportionately close to the United States on the map showing Reuben's body in space.",
+
+      "Morty told Annie that she said his age as 12, when it was actually Poncho who said that moments before.",
+    ],
+    id: 3,
+    image: 'https://i.pinimg.com/originals/fb/da/e0/fbdae0349a52c4cab6c79c8769bd7bdf.jpg',
+    locations: [
+      'Smith Residence',
+      'Anatomy Park',
+    ],
+    name: 'Anatomy Park',
+    synopsis: "Morty ends up at a bizarre theme park of infectious diseases located inside of a homeless man. Jerry's parents introduce their new mutual lover.",
+    trivia: [
+      "This is the first holiday-themed episode of the series.",
+
+      "According to the DVD commentary, Anatomy Park's intended demographic was other members of Dr. Xenon Bloom's species.",
+
+      "Summer has apparently moved on from her previous crush Frank Palicky's death as she was seen with a boyfriend named Ethan who she has apparently been dating for some time.",
+
+      "Although episodes produced before and after this one feature Morty's central crush and love interest being Jessica, he develops an interest in Annie during this episode. As Rick shrinks her down again and sends her inside Ethan, Morty protests.",
+    ],
+  },
+  // Season 1 - Episode 4
+  {
+    deaths: [
+      "Prince Nebulon",
+      "Kevin",
+      "Cynthia",
+      "Stu",
+      "All known Zigerions (Extinction)",
+      "Many imprisoned aliens on the ship",
+    ],
+    description: '"M. Night Shaym-Aliens!" is the fourth episode of the first season of Rick and Morty. It is the fourth episode of the series overall. It premiered on January 13, 2014. It was written by Tom Kauffman and directed by Jeff Myers.',
+    errors: [
+      "Rick shoves his and Morty's clothes into a sewer to prevent Morty from getting dressed. However, the next scene the two appear in, they are again wearing their standard attire (with some small adjustments to match the rap/hip-hop theme of the 'concert' they're putting on). No explanation is given as to how the two are able to find clothes again.",
+
+      "When preparing to do their hip-hop concert, Rick's shirt is untucked on the right side, and the clock on Morty's necklace is set to 9:00 (or 11:45). When they go on stage, Rick's shirt is untucked on the left side, and Morty's clock is set to 3:00 (or 12:15). Also, when the camera switches to up stage, Morty's watch changes from his right arm to his left.",
+    ],
+    id: 4,
+    image: 'https://i.pinimg.com/originals/39/6d/fa/396dfa909731e6a9e42f1abf44fb1fc8.jpg',
+    locations: [
+      "Smith Residence",
+      "Haas & Milan",
+      "Holographic Simulation",
+      "Harry Herpson High School (hologram only)",
+    ],
+    name: 'M. Night Shaym-Aliens!',
+    synopsis: 'Aliens send Rick, Morty and Jerry into an alternate reality, and Rick tries to get them out as oblivious Jerry pitches a marketing slogan for apples.',
+    trivia: [
+      "The episode's name is based on the movie director M. Night Shyamalan, who's known for his continuous use of story-line twists.",
+
+      'The song played throughout the episode and during the credits is an instrumental version of "Baker Street" by Gerry Rafferty.',
+
+      "A cameo of a Pop-tart is seen in episode driving a toaster to work.",
+
+      "Jerry losing his job in this episode would be continued in the episode Close Rick-Counters of the Rick Kind, where he would be looking for job opportunities on the internet and telling Doofus Rick that he's 'In between advertising jobs' and again in Look Who's Purging Now, where Jerry starts begging Summer to float him some money making her yell at him to get a job. He would later be heard talking to a guy on the phone whose job is to just talk to lonely people, so that Beth could tell him the same thing.",
+
+      'According to commentary on the Season 1 DVD, the scene in which Morty is in class was originally going to end in an orgy.',
+      
+      "Prince Nebulon is seen mixing the Plutonic Quartz and Cesium in the wrong order and quantity, which is shown through the bottles as opposed to Simulation Morty's mix at the ship. It's unclear if this is an error or just recklessness in Prince Nebulon's part.",
+
+      "The main reason why the ship blew up was because cesium is highly reactive and explosive when coming in contact with water. However, with the 2 parts of Plutonic Quartz, the explosion was even worse.",
+
+      "The Zigerions are references to Nigerian online scammers. Not only do the terms rhyme but Rick describes the Zigerions as '... the galaxy's most ambitious and least successful con artists...'",
+
+      "The girl mindlessly bouncing a red rubber ball outside of Jerry's house is a reference to a scene in the novel A Wrinkle in Time by Madeleine L'Engle.",
+
+      "The floating char that can be seen as Rick, Morty and Jerry flee through the Zigerion lab is probably based on the kinetic artwork 'Cory's Yellow Chair' by Arthur Ganson.",
+
+      "The Zigerions are very similar to the Scammer characters from the Futurama movie, Bender's Big Score.",
+
+      'The "glitchy" effect that all the characters have in the simulated world is based on cartridge tilting, which references the glitches in the gameplay of the video game, GoldenEye.',
+
+      "The doors on the alien ship make the sound used in Star Trek: The Original Series.",
+    ],
+  },
+  // Season 1 - Episode 5
+  {
+    deaths: [
+      "Evil Beth Clone",
+      "Evil Jerry Clone",
+      "Evil Summer Clone",
+      "Dale",
+      "Mr. Jellybean",
+      "Many Meeseeks",
+    ],
+    description: `"Meeseeks and Destroy" is the fifth episode of the first season of Rick and Morty. It is the fifth episode of the series overall. It premiered on January 20, 2014. It was written by Ryan Ridley and directed by Bryan Newton.`,
+    errors: [
+      'unknown'
+    ],
+    id: 5,
+    image: `https://i.pinimg.com/originals/39/6d/fa/396dfa909731e6a9e42f1abf44fb1fc8.jpg`,
+    locations: [
+      `Smith Residence`,
+      `Harry Herpson High School`,
+      `Giant Court`,
+      `Fancy Eats`,
+      `Thirsty Step`,
+    ],
+    name: `Meeseeks and Destroy`,
+    songs: [
+      `Sweet Home Alabama`,
+    ],
+    synopsis: `Morty has grown weary of Rick's shenanigans, so he leads a supposedly safer adventure. Jerry conjures weird creatures to help him with his golf game.`,
+    trivia: [
+      `This is the episode where Rick invents his catchphrase "Wubba Lubba Dub Dub!"`,
+      `This episode shows some of Rick's affection towards Morty by killing King Jellybean for his actions against Morty.`,
+    ],
+  },
+  // Season 1 - Episode 6
+  {
+    deaths: [''],
+    description: '',
+    errors: [''],
+    id: 6,
+    image: '',
+    locations: [''],
+    name: 'Anatomy Park',
+    synopsis: '',
+    trivia: [''],
+  },
+  // Season 1 - Episode 7
+  {
+    deaths: [''],
+    description: '',
+    errors: [''],
+    id: 7,
+    image: '',
+    locations: [''],
+    name: 'Anatomy Park',
+    synopsis: '',
+    trivia: [''],
+  },
+  // Season 1 - Episode 8
+  {
+    deaths: [''],
+    description: '',
+    errors: [''],
+    id: 8,
+    image: '',
+    locations: [''],
+    name: 'Anatomy Park',
+    synopsis: '',
+    trivia: [''],
+  },
+  // Season 1 - Episode 9
+  {
+    deaths: [''],
+    description: '',
+    errors: [''],
+    id: 9,
+    image: '',
+    locations: [''],
+    name: 'Anatomy Park',
+    synopsis: '',
+    trivia: [''],
+  },
+  // Season 1 - Episode 10
+  {
+    deaths: [''],
+    description: '',
+    errors: [''],
+    id: 10,
+    image: '',
+    locations: [''],
+    name: 'Anatomy Park',
+    synopsis: '',
+    trivia: [''],
+  },
+  // Season 1 - Episode 11
+  {
+    deaths: [''],
+    description: '',
+    errors: [''],
+    id: 11,
+    image: '',
+    locations: [''],
+    name: 'Anatomy Park',
+    synopsis: '',
+    trivia: [''],
+  },
+];
+
+export default EpisodesAddOn;
