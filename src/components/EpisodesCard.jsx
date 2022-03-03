1e4a83b59b2f765160bad79638cbaf84ff5cbc29
lbcamargo94 <lb.camargo94@gmail.com>
2022-03-03 16:01:53 -0300

    feat:Episodes.jsx, EpisodesCard.jsx: Adicionado componentes de cards e paginaçao no arquivo Episodes, Criado novo arquivo EpisodesCard ✅

---
+++ src/components/EpisodesCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EpisodesCard() {
+  return (
+    <div></div>
+  )
+}
--- src/pages/Episodes.jsx
+++ src/pages/Episodes.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import EpisodesCard from '../components/EpisodesCard';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import Paginations from '../components/Paginations';
+import * as api from '../services/api';
 
 export default function Episodes() {
+
+  const [episodes, setEpisodes] = useState();
+  const [currentPage, setCurrentPage] = useState(1)
+  const [pageLimit, setPageLimit] = useState();
+  const [loading, setLoading] = useState(true);
+  
+  async function loadEpisodes(path){
+    setLoading(true);
+    const result = await api.getCharacters(`/episode?page=${ path }`);
+    setEpisodes(result.data);
+    setPageLimit(result.data.info.pages);
+    setLoading(false);
+  }
+  
+  const pageNumbers = [];
+  for (let ind = 1; ind <= pageLimit; ind += 1){
+    pageNumbers.push(ind);
+  }
+  
+  useEffect(()=> {
+    loadEpisodes('')
+  },[]);
+  
+  console.log(episodes);
+
   return (
     <div>
       <Header />
+      <section>
+        <Paginations
+          locations={ episodes }
+          pageLimit={ pageLimit }
+          currentPage={ currentPage }
+          setCurrentPage={ setCurrentPage }
+          loadCharacters= { loadEpisodes }
+        />
+      </section>
+      <section
+        className="align-items-center align-self-stretch bg-success
+        d-flex flex-wrap justify-content-evenly p-3 w-100 h-100"
+      >
+      { loading ? <Loading /> : { ...episodes }.results.map((index) =>
+        <EpisodesCard key={ index.id } { ...index }/>) }
+      </section>
+      <section>
+        <Paginations
+          locations={ episodes }
+          pageLimit={ pageLimit }
+          currentPage={ currentPage }
+          setCurrentPage={ setCurrentPage }
+          loadCharacters= { loadEpisodes }
+        />
+      </section>
     </div>
   );
 }
