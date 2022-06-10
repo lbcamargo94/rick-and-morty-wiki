f70e3c8e0f02e402d0995c82e3851bea3ac260d6
lucasmonstro <joao.galiano.silva@gmail.com>
2022-06-10 16:53:58 +0100

    :recycle: move assets into assets folder

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import icon from '../icons/icons8-rick-sanchez.svg';
+import icon from '../assets/icons/icons8-rick-sanchez.svg';
 import '../styles/Loading.css';
 import '../styles/App.css';
 
 export default function Loading() {
   return (
-    <div className="d-flex flex-row justify-content-center align-items-center vw-100 vh-100">
-      <img className="spinner-icon"
-        src={ icon }
-        alt="icons8-rick-sanchez.svg"
-      />
-      <h1 className="d-flex fadeOut align-items-center d-flex m-0 mx-3">
+    <div className='d-flex flex-row justify-content-center align-items-center vw-100 vh-100'>
+      <img className='spinner-icon' src={icon} alt='icons8-rick-sanchez.svg' />
+      <h1 className='d-flex fadeOut align-items-center d-flex m-0 mx-3'>
         Loading
-        <p className="bounce one d-flex fs-1">.</p>
-        <p className="bounce two d-flex fs-1">.</p>
-        <p className="bounce three d-flex fs-1">.</p>
+        <p className='bounce one d-flex fs-1'>.</p>
+        <p className='bounce two d-flex fs-1'>.</p>
+        <p className='bounce three d-flex fs-1'>.</p>
       </h1>
     </div>
   );
--- src/components/MainCarousel.jsx
+++ src/components/MainCarousel.jsx
@@ -1,106 +1,86 @@
 import React from 'react';
 import { Carousel, Nav } from 'react-bootstrap';
+
+import imageCharacters from '../assets/image/Characters.png';
+import imageLocations from '../assets/image/Locations.png';
+import imageEpisodes from '../assets/image/Episodes.png';
 import { useUpdateContext } from '../Utils/Provider';
-
-import imageCharacters from '../image/Characters.png';
-import imageLocations from '../image/Locations.png';
-import imageEpisodes from '../image/Episodes.png'
-
-import '../styles/MainCarousel.css'
+import '../styles/MainCarousel.css';
 
 export default function MainCarousel() {
   const { data } = useUpdateContext();
   const { info } = data.data.characters;
-  const currentPage = info ? info.next.replace('https://rickandmortyapi.com/api/character?page=', '') : '';
+  const currentPage = info
+    ? info.next.replace('https://rickandmortyapi.com/api/character?page=', '')
+    : '';
 
   return (
     <div
-      className="align-items-start bg-success d-flex flex-row
-      justify-content-center h-100 w-100"
+      className='align-items-start bg-success d-flex flex-row
+      justify-content-center h-100 w-100'
     >
-      <Carousel
-        fade
-      >
+      <Carousel fade>
         <Carousel.Item
-          className="d-flex flex-column align-items-center
-          justify-content-center p-3"
+          className='d-flex flex-column align-items-center
+          justify-content-center p-3'
         >
           <Nav.Link
-            href={`/characters/${ currentPage-1 }`}
-            className="d-flex h-100 p-0 rounded-3 text-info border
-            border-light"
+            href={`/characters/${currentPage - 1}`}
+            className='d-flex h-100 p-0 rounded-3 text-info border
+            border-light'
           >
             <img
-            alt="First slide"
-            className="d-flex h-100 rounded-3 text-info w-100
-            carousel-image-category"
-            src={ imageCharacters ? imageCharacters : '' }
+              alt='First slide'
+              className='d-flex h-100 rounded-3 text-info w-100
+            carousel-image-category'
+              src={imageCharacters ? imageCharacters : ''}
             />
           </Nav.Link>
-          <Carousel.Caption 
-            className="bg-dark h-auto m-auto opacity-75 p-1 text-light carousel-title-category"
-          >
-            <h3
-              className="h-50 m-auto w-75 fs-3"
-            >
-              Characters
-            </h3>
+          <Carousel.Caption className='bg-dark h-auto m-auto opacity-75 p-1 text-light carousel-title-category'>
+            <h3 className='h-50 m-auto w-75 fs-3'>Characters</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item
-          className="d-flex flex-column align-items-center
-          justify-content-center p-3"
+          className='d-flex flex-column align-items-center
+          justify-content-center p-3'
         >
           <Nav.Link
-            href={`/locations/${ currentPage-1 }`}
-            className="d-flex h-100 p-0 rounded-3 text-info border
-            border-light"
+            href={`/locations/${currentPage - 1}`}
+            className='d-flex h-100 p-0 rounded-3 text-info border
+            border-light'
           >
             <img
-              alt="Second slide"
-              className="d-flex h-100 rounded-3 text-info w-100 mx-auto my-0
-              carousel-image-category"
-              src={ imageLocations ? imageLocations : '' }
+              alt='Second slide'
+              className='d-flex h-100 rounded-3 text-info w-100 mx-auto my-0
+              carousel-image-category'
+              src={imageLocations ? imageLocations : ''}
             />
           </Nav.Link>
-          <Carousel.Caption
-            className="bg-dark h-auto m-auto opacity-75 p-1 text-light carousel-title-category"
-          >
-            <h3
-              className="h-50 m-auto w-75 fs-3"
-            >
-              Locations
-            </h3>
+          <Carousel.Caption className='bg-dark h-auto m-auto opacity-75 p-1 text-light carousel-title-category'>
+            <h3 className='h-50 m-auto w-75 fs-3'>Locations</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item
-          className="d-flex flex-column align-items-center
-          justify-content-center p-3"
+          className='d-flex flex-column align-items-center
+          justify-content-center p-3'
         >
           <Nav.Link
-            href={`/episodes/${ currentPage-1 }`}
-            className="d-flex h-100 p-0 rounded-3 text-info border
-            border-light"
+            href={`/episodes/${currentPage - 1}`}
+            className='d-flex h-100 p-0 rounded-3 text-info border
+            border-light'
           >
             <img
-              alt="Third slide"
-              className="d-flex h-100 rounded-3 text-info w-100 mx-auto my-0
-              carousel-image-category"
-              src={ imageEpisodes ? imageEpisodes : '' }
+              alt='Third slide'
+              className='d-flex h-100 rounded-3 text-info w-100 mx-auto my-0
+              carousel-image-category'
+              src={imageEpisodes ? imageEpisodes : ''}
             />
           </Nav.Link>
-        <Carousel.Caption
-          className="bg-dark h-auto m-auto opacity-75 p-1 text-light carousel-title-category"
-        >
-          <h3
-            className="h-50 m-auto w-75 fs-3"
-          >
-            Episodes
-          </h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          <Carousel.Caption className='bg-dark h-auto m-auto opacity-75 p-1 text-light carousel-title-category'>
+            <h3 className='h-50 m-auto w-75 fs-3'>Episodes</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
-
